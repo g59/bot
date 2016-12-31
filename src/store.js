54f52d4b9dac2dcba504000006a25f013fce9d78
Keisuke Kan <9renpoto@gmail.com>
2016-12-31 22:07:30 +0900

    chore(test) add set test

--- src/store.js
+++ src/store.js
@@ -1,21 +1,56 @@
 /* @flow */
 const redis = require('redis')
+const random = require('lodash').random
+
+type Callback = (cnt: number) => void
+
+const MEMBER = 'KARMASTORE'
+const zadd = (client: redis, key, n, cb: Callback) => {
+  client.zadd([key, n, MEMBER], (err, res) => {
+    if (err) {
+      console.error(err)
+    }
+    cb(res)
+  })
+}
+
+const zincrby = (client: redis, key: string, n: number, cb: Callback) => {
+  client.zincrby([key, n, MEMBER], (err, res) => {
+    if (err) {
+      console.error(err)
+    }
+    cb(res)
+  })
+}
 
 module.exports = class Karma {
+  MEMBER: string
   client: redis
   constructor () {
-    this.client = redis.createClient()
+    this.client = redis.createClient(process.env.REDIS_URL)
     this.client.on('error', err => {
-      console.log('Error ' + err)
+      if (err) {
+        throw err
+      }
     })
   }
-  up (thing: string, n: number): number {
-    return 1
+  up (key: string, n: number, cb: Callback) {
+    this.client.zscore(key, MEMBER, (err, res) => {
+      if (err) {
+        console.error(err)
+      }
+      console.log(res)
+      if (res) {
+        zincrby(this.client, key, res + n, cb)
+        return
+      }
+      zadd(this.client, key, n, cb)
+    })
   }
-  down (thing: string, n: number): number {
-    return 1
+  down (key: string, n: number, cb: Callback) {
+    this.up(key, n * -1, cb)
   }
-  random (thing: string, min: number = 1, max: number = 1): number {
-    return 1
+  random (key: string, min: number = 1, max: number = 1, cb: Callback) {
+    this.up(key, random(min, max), cb)
   }
 }
--- test/store.test.js
+++ test/store.test.js
@@ -1,8 +1,24 @@
 const assert = require('assert')
+const random = require('faker').random
 
 describe('store', () => {
   const Store = require('../src/store')
-  it('load', () => [
+  it('load', () => {
     assert(Store)
-  ])
+  })
+
+  it('up', done => {
+    const s = new Store()
+    assert(typeof s.up === 'function')
+    const score = 1
+    const key = random.word()
+    s.up(key, score, d => {
+      assert(d === score)
+    })
+
+    s.up(key, score, d => {
+      assert(d === score * 2)
+      done()
+    })
+  })
 })
