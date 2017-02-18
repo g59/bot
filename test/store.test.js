de4e4ae25a4ddd57224d2086065f0549e6e3e43d
Keisuke Kan <9renpoto@gmail.com>
2017-02-18 18:58:28 +0900

    chore(src) set data

--- test/store.test.js
+++ test/store.test.js
@@ -2,12 +2,27 @@ import test from 'ava'
 import { random } from 'faker'
 import Store from '../src/store'
 
-test('store up', t => {
-  const s = new Store()
+test('store', t => {
+  const s = new Store(random.word())
   t.is(typeof s.up, 'function')
-  const score = 1
-  const key = random.word()
+
+  const score = random.number()
+
+  let key = random.word()
   s.up(key, score, d => {
     t.is(d, score)
+  })
+
+  s.up(key, score, d => {
+    t.is(d, score + score)
+  })
+
+  s.down(key, score, d => {
+    t.is(d, score - score)
+  })
+
+  key = random.word()
+  s.random(key, d => {
+    t.true(d > 0)
   })
 })
