46b958fc91f6bc33bf5a94404f020ed19833dfb8
Keisuke Kan <9renpoto@gmail.com>
2018-12-12 19:34:29 +0900

    Added any types... (#169)

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-module.exports = {
-  extends: ['@9renpoto/eslint-config-flowtype'],
-  rules: {
-    'no-undef': 1,
-    'valid-jsdoc': 0
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,93 +1,71 @@
-/* @flow */
-import { WebClient } from '@slack/client'
-import moment from 'moment'
-
-type PostOptions = {
-  color: string,
-  image_url: string,
-  text: string,
-  title_link: string,
-  title: string
-}
-
-const defaultOptions: PostOptions = {
-  color: 'warning',
-  image_url:
-    'https://68.media.tumblr.com/adac611ed299098bbfd5527cacb1feb5/tumblr_o6v2n6Dwqn1vp0h0go1_500.gif',
-  text: 'if not active',
-  title_link: 'https://get.slack.help/hc/en-us/articles/201563847',
-  title: 'Please archive this channel'
-}
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@slack/client");
+const moment_1 = __importDefault(require("moment"));
+const defaultOptions = {
+    color: 'warning',
+    image_url: 'https://68.media.tumblr.com/adac611ed299098bbfd5527cacb1feb5/tumblr_o6v2n6Dwqn1vp0h0go1_500.gif',
+    text: 'if not active',
+    title_link: 'https://get.slack.help/hc/en-us/articles/201563847',
+    title: 'Please archive this channel'
+};
 /**
  * @name ActiveReminder
  * @param {number} MAX_DATES
  * @param {any} POST_OPTIONS
  * @param {any} attachments
  */
-export default class ActiveReminder {
-  _web: WebClient
-  _MAX_DATES: number
-  _POST_OPTIONS: PostOptions
-  _attachments: any
-  constructor (
-    MAX_DATES: number = 14,
-    POST_OPTIONS: any = {},
-    attachments: any = {}
-  ) {
-    const token = process.env.SLACK_API_TOKEN
-    if (!token) {
-      throw new Error()
+class ActiveReminder {
+    constructor(MAX_DATES = 14, POST_OPTIONS = {}, attachments = {}) {
+        this._web = new client_1.WebClient(process.env.SLACK_API_TOKEN);
+        this._MAX_DATES = MAX_DATES;
+        this._POST_OPTIONS = POST_OPTIONS;
+        this._attachments = attachments;
     }
-    this._web = new WebClient(token)
-    this._MAX_DATES = MAX_DATES
-    this._POST_OPTIONS = POST_OPTIONS
-    this._attachments = attachments
-  }
-  /**
-   * post remind message
-   * @return {Promise} channels.history
-   */
-  async postRemindMessage () {
-    const { channels } = await this._web.channels.list({
-      exclude_archived: true
-    })
-    channels.map(({ id }) => {
-      this.checkLastMessage(id)
-    })
-  }
-  /**
-   * check last message timestamp
-   * @param  {string}  id channel.id
-   * @return {Promise}    chat.postMessage
-   */
-  async checkLastMessage (id: string) {
-    const { messages } = await this._web.channels.history(id, { count: 1 })
-    const last = moment.unix(messages[0].ts)
-    if (
-      moment()
-        .subtract(this._MAX_DATES, 'days')
-        .isAfter(last)
-    ) {
-      this.postMessage(id)
+    /**
+     * post remind message
+     * @return {Promise} channels.history
+     */
+    async postRemindMessage() {
+        const { channels } = await this._web.channels.list({
+            exclude_archived: true
+        });
+        channels.map(({ id }) => this.checkLastMessage(id));
     }
-  }
-  /**
-   * post slack messages
-   * @param  {string}  id channel.id
-   * @return {Promise}    chat.postMessage
-   */
-  async postMessage (id: string) {
-    const options = {
-      ...defaultOptions,
-      ...this._POST_OPTIONS
+    /**
+     * check last message timestamp
+     * @param  {string}  id channel.id
+     * @return {Promise}    chat.postMessage
+     */
+    async checkLastMessage(id) {
+        const { messages } = await this._web.channels.history(id, { count: 1 });
+        const last = moment_1.default.unix(messages[0].ts);
+        if (moment_1.default()
+            .subtract(this._MAX_DATES, 'days')
+            .isAfter(last)) {
+            this.postMessage(id);
+        }
     }
-
-    await this._web.chat.postMessage(id, '', {
-      icon_emoji: ':video_game:',
-      username: 'solaire',
-      attachments: `${JSON.stringify([options])}`,
-      ...this._attachments
-    })
-  }
+    /**
+     * post slack messages
+     * @param  {string}  id channel.id
+     * @return {Promise}    chat.postMessage
+     */
+    async postMessage(id) {
+        const options = {
+            ...defaultOptions,
+            ...this._POST_OPTIONS
+        };
+        await this._web.chat.postMessage(id, '', {
+            icon_emoji: ':video_game:',
+            username: 'solaire',
+            attachments: `${JSON.stringify([options])}`,
+            ...this._attachments
+        });
+    }
 }
+exports.default = ActiveReminder;
+//# sourceMappingURL=index.js.map
