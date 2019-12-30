e8f78a668e3497f6e63de7ed7d0c75f9293ac323
Keisuke Kan <9renpoto@gmail.com>
2019-12-30 21:45:38 +0900

    chore: Added secrets

--- examples/botkit-karma-simple/src/slack.ts
+++ examples/botkit-karma-simple/src/slack.ts
@@ -3,7 +3,11 @@ import KarmaBot from "botkit-karma-simple";
 import { SlackAdapter } from "botbuilder-adapter-slack";
 
 const adapter = new SlackAdapter({
-  botToken: process.env.SLACK_API_TOKEN
+  botToken: process.env.SLACK_API_TOKEN,
+  clientSecret: process.env.clientSecret,
+  clientSigningSecret: process.env.clientSigningSecret,
+  verificationToken: process.env.verificationToken,
+  redirectUri: `https://${process.env.SLACK_DOMAIN}/install/auth`
 });
 const controller = new Botkit({ adapter });
 
