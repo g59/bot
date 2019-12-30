b2619d684e180a589802e8e5ed8fb0a5c67f4c2b
Keisuke Kan <9renpoto@gmail.com>
2019-12-30 18:25:46 +0900

    fix: use jest

--- packages/karma-store-redis/README.md
+++ packages/karma-store-redis/README.md
@@ -1,14 +1,18 @@
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*
+
+- [karma-store-redis](#karma-store-redis)
+  - [Install](#install)
+  - [License](#license)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
 # karma-store-redis
 
 Karma Store
 
 [![npm version](https://badge.fury.io/js/karma-store-redis.svg)](https://badge.fury.io/js/karma-store-redis)
-[![Build Status][travis-image]][travis-url]
-[![codecov][codecov-image]][codecov-url]
-[![Dependency Status][daviddm-image]][daviddm-url]
-[![devDependency Status](https://david-dm.org/9renpoto/karma-store-redis/dev-status.svg)](https://david-dm.org/9renpoto/karma-store-redis/dev-status.svg)
-[![codebeat badge](https://codebeat.co/badges/01011aaa-643b-4f40-a5f1-6f125f77500c)](https://codebeat.co/projects/github-com-9renpoto-karma-store-redis-master)
-[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2F9renpoto%2Fkarma-store-redis.svg?type=shield)](https://app.fossa.io/projects/git%2Bgithub.com%2F9renpoto%2Fkarma-store-redis?ref=badge_shield)
 
 ## Install
 
@@ -19,12 +23,3 @@ npm install karma-store-redis --save
 ## License
 
 MIT
-
-[travis-image]: https://travis-ci.org/9renpoto/karma-store-redis.svg?branch=master
-[travis-url]: https://travis-ci.org/9renpoto/karma-store-redis
-[codecov-image]: https://codecov.io/gh/9renpoto/karma-store-redis/branch/master/graph/badge.svg
-[codecov-url]: https://codecov.io/gh/9renpoto/karma-store-redis
-[daviddm-image]: https://david-dm.org/9renpoto/karma-store-redis.svg?theme=shields.io
-[daviddm-url]: https://david-dm.org/9renpoto/karma-store-redis
-
-[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2F9renpoto%2Fkarma-store-redis.svg?type=large)](https://app.fossa.io/projects/git%2Bgithub.com%2F9renpoto%2Fkarma-store-redis?ref=badge_large)
--- packages/karma-store-redis/test/store.test.ts
+++ packages/karma-store-redis/test/store.test.ts
@@ -1,66 +1,65 @@
-import assert from 'assert'
-import { random } from 'faker'
-import Store from '../src/store'
+import { random } from "faker";
+import Store from "../src/store";
 
-describe('Store', () => {
-  let s: Store
+describe("Store", () => {
+  let s: Store;
   beforeEach(() => {
-    s = new Store(random.alphaNumeric(30))
-  })
+    s = new Store(random.alphaNumeric(30));
+  });
 
   afterEach(() => {
-    s.clearAll(s._key())
-    s.quit()
-  })
+    s.clearAll(s._key());
+    s.quit();
+  });
 
-  it('after clear', done =>
+  it("after clear", done =>
     s.top(-1, (err, rank) => {
-      assert(rank.length === 0)
-      assert(err === null)
-      done()
-    }))
+      expect(rank).toHaveLength(0);
+      expect(err === null).toBeTruthy();
+      done();
+    }));
 
-  it('random', done =>
+  it("random", done =>
     s.random(random.alphaNumeric(), (err, res) => {
-      assert(parseInt(res, 10) === 1)
-      assert(err === null)
-      done()
-    }))
+      expect(parseInt(res, 10) === 1).toBeTruthy();
+      expect(err === null).toBeTruthy();
+      done();
+    }));
 
-  it('++ & --', done => {
-    const key = random.alphaNumeric()
-    const score1 = random.number({ max: 10 })
-    s.up(key, score1, (_, d) => assert(d === score1.toString()))
+  it("++ & --", done => {
+    const key = random.alphaNumeric();
+    const score1 = random.number({ max: 10 });
+    s.up(key, score1, (_, d) => expect(d === score1.toString()).toBeTruthy());
 
-    const score2 = random.number({ min: 0, max: 10 })
+    const score2 = random.number({ min: 0, max: 10 });
     s.up(key, score2, (err, now) => {
-      assert(now === (score1 + score2).toString())
-      assert(err === null)
+      expect(now === (score1 + score2).toString()).toBeTruthy();
+      expect(err === null).toBeTruthy();
       s.down(key, score2, (err2, next) => {
-        assert(next === score1.toString())
-        assert(err2 === null)
-      })
-    })
+        expect(next === score1.toString()).toBeTruthy();
+        expect(err2 === null).toBeTruthy();
+      });
+    });
 
     s.top(-1, (err, rank) => {
-      assert(rank.length === 2)
-      assert(err === null)
-      const max = parseInt(rank[1], 10)
+      expect(rank).toHaveLength(2);
+      expect(err).toBeNull();
+      const max = parseInt(rank[1], 10);
       for (let i = 0; i < rank.length; i += 2) {
-        assert(typeof rank[i] === 'string')
-        assert(parseInt(rank[i + 1], 10) <= max)
+        expect(typeof rank[i]).toBe("string");
+        expect(parseInt(rank[i + 1], 10) <= max).toBeTruthy();
       }
-    })
+    });
 
     s.lowest(-1, (err, rank) => {
-      assert(rank.length === 2)
-      assert(err === null)
-      const min = parseInt(rank[1], 10)
+      expect(rank).toHaveLength(2);
+      expect(err === null).toBeTruthy();
+      const min = parseInt(rank[1], 10);
       for (let i = 0; i < rank.length; i += 2) {
-        assert(typeof rank[i] === 'string')
-        assert(parseInt(rank[i + 1], 10) >= min)
+        expect(typeof rank[i] === "string").toBeTruthy();
+        expect(parseInt(rank[i + 1], 10) >= min).toBeTruthy();
       }
-      done()
-    })
-  })
-})
+      done();
+    });
+  });
+});
